cd4efd29d31045360d2b591d0e76bec5fd6a84fd
Filip Barankiewicz <filip.barankiewicz@crestt.pl>
2019-10-04 21:48:05 +0200

    Added an empty project

---
+++ HRpest/HRpest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRpest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

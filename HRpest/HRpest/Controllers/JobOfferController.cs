4ca45780dec257ba1fd675a4bf43b5ed5bfd29d0
Filip Barankiewicz <filip.barankiewicz@crestt.pl>
2019-10-20 15:48:42 +0200

    Add classes from the tutorial, update bootstrap

---
+++ HRpest/HRpest/Controllers/JobOfferController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+	[Route("[controller]")]
+	public class JobOfferController : Controller
+	{
+		private static List<JobOffer> _jobOffers = new List<JobOffer>
+		{
+			new JobOffer{Id=1, JobTitle= "Backend Developer" },
+			new JobOffer{Id=1, JobTitle= "Frontend Developer" },
+			new JobOffer{Id=1, JobTitle= "Manager" },
+			new JobOffer{Id=1, JobTitle= "Cook" }
+		};
+
+		[Route("index")]
+		public IActionResult Index()
+		{
+			return View(_jobOffers);
+		}
+		public IActionResult Details(int id)
+		{
+			var offer = _jobOffers.FirstOrDefault(o => o.Id == id);
+			return View(offer);
+		}
+
+	}
+}

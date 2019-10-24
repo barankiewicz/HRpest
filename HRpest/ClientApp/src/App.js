af4909688463bc9764f8739928c790fffc8fab9f
Filip Barankiewicz <filip.barankiewicz@crestt.pl>
2019-10-24 20:46:27 +0200

    Login frontend #107

--- HRpest/ClientApp/src/App.js
+++ HRpest/ClientApp/src/App.js
@@ -1,17 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Login } from './components/Login';
+import WrappedNormalLoginForm from './components/Login';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import 'antd/dist/antd.css';
+
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
     return (
-      <Login />
+      <WrappedNormalLoginForm />
       // <Layout>
       //   <Route exact path='/' component={Home} />
       //   <Route path='/login' component={Login} />
--- HRpest/ClientApp/src/components/Login.js
+++ HRpest/ClientApp/src/components/Login.js
@@ -1,26 +1,68 @@
-import React, { Component } from 'react';
-import { Row, Col } from 'antd';
-import { Input } from 'antd';
-import { Typography } from 'antd';
-import 'antd/dist/antd.css';
-const { Title } = Typography;
-export class Login extends Component {
+import React, { Component } from 'react'
+import { Form, Icon, Input, Button, Checkbox, Row, Typography } from 'antd';
+import './Login.css';
 
-  render () {
+const { Title } = Typography;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>
-        <Row type="flex" justify="space-around" align="middle">
-          <Col span={4} offset={10}>
-            <Title>HRpest</Title>
-          </Col>
-        </Row>
-        <Row type="flex" justify="space-around" align="middle">
-          <Col span={4} offset={10}>
-            <Input placeholder="Username" />
-            <Input.Password placeholder="Password" />
-          </Col>
-        </Row>
-    </div>
+      <div id = "center">
+        <Row type="flex" justify="space-around" align="middle"><Title strong>HRpest</Title></Row>
+      <Row type="flex" justify="space-around" align="middle">
+        <Form onSubmit={this.handleSubmit} className="login-form" >
+          <Form.Item>
+            {getFieldDecorator('username', {
+              rules: [{ required: true, message: 'Please input your username!' }
+                      ],
+            })(
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('password', {
+              rules: [{ required: true, message: 'Please input your Password!' },
+                  { min: 6, message: 'Your username must be at least 8 characters long!' }],
+            })(
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="Password"
+              />,
+            )}
+          </Form.Item>
+          <Form.Item>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true,
+            })(<Checkbox>Remember me</Checkbox>)}
+            <a className="login-form-forgot" href="">
+              Forgot password
+          </a>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+          </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </Row>
+      </div>
     );
   }
 }
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
+export default WrappedNormalLoginForm;

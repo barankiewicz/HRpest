3405f1ec194eaabcb598109655130d8b71365155
Filip Barankiewicz <filip.barankiewicz@crestt.pl>
2019-10-27 07:22:07 +0100

    Sign up WIP #120 #111

--- HRpest.APP/Startup.cs
+++ HRpest.APP/Startup.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace HRpest
+namespace HRpest.APP
 {
     public class Startup
     {
@@ -20,7 +20,8 @@ namespace HRpest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -30,7 +31,7 @@ namespace HRpest
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -47,11 +48,13 @@ namespace HRpest
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
---
+++ HRpest.DAL/Class/HrPestContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using HRpest.BL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace HRpest.DAL.Class
+{
+    public class HrPestContext : DbContext
+    {
+        public HrPestContext(DbContextOptions<HrPestContext> options) : base(options) { }
+        public virtual DbSet<User> Users { get; set; }
+    }
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<HrPestContext>
+    {
+        public HrPestContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../HRPest.APP/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<HrPestContext>();
+            var connectionString = configuration.GetConnectionString("DatabaseConnection");
+            builder.UseSqlServer(connectionString);
+            return new HrPestContext(builder.Options);
+        }
+    }
+}
